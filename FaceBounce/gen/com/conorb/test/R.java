ed54d557489638fd468256bb7c416c7882d2e479
Conor Byrne <conorb@gmail.com>
2013-12-03 10:20:33 +0000

    Fixed project target

--- FaceBounce/gen/com/conorb/test/R.java
+++ FaceBounce/gen/com/conorb/test/R.java
@@ -16,14 +16,10 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int FBView=0x7f060003;
-        public static final int imageView1=0x7f060000;
-        public static final int textView1=0x7f060001;
-        public static final int textView2=0x7f060002;
+        public static final int FBView=0x7f060000;
     }
     public static final class layout {
-        public static final int intro=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class raw {
         public static final int tt=0x7f040000;
